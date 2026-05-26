18996a1a0faf91d46e48ef6543d04c52fbd229e8
unknown <gmumoltd@gmail.com>
2026-05-26 15:08:20 +0300

    first commit

---
+++ First_PHP_work_folder/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+<div class ="container"> 
+    <h1>Running PHP</h1>
+    <form action="" method="post">
+        <label for="username">Username</label>
+        <input type="text" placeholder="Enter username" name="username">
+        <button type="submit">Submit</button>
+    </form>
+</div>
+
+<?php
+echo "Hello Students <br>";
+
+// variables in php
+ 
+$name = "GEoffrey";
+
+$age = 26;
+
+$work = "Student at FCA";
+
+echo "$name <br>";
+echo "$age<br>" ;
+echo "$work<br>";
+// string Concatination  combining text 
+echo "My Name is <br>" . $name;
+
+
+// handling the form submission
+$username = $_POST["username"];
+echo $username;
+
+?>
+</body>
+</html>
